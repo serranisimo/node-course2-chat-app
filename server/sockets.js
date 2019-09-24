881e3644dee216332546c6f82d00adb31f1a6287
serranisimo <9xdbdRqzPskzf6x9TNsH>
2019-09-24 08:05:55 +0200

    refactoring

--- server/sockets.js
+++ server/sockets.js
@@ -11,6 +11,9 @@ const {
     Users
 } = require('./utils/users');
 
+var socket, io;
+var users = new Users();
+
 /**
  * socket.leave(room) --> leaves a room
  * 
@@ -22,75 +25,87 @@ const {
  */
 
 module.exports.socketRoutes = (server) => {
-    var io = socketio(server);
-    var users = new Users();
+    io = socketio(server);
 
-    io.on("connection", (socket) => {
+    io.on("connection", (inputSocket) => {
+        socket = inputSocket;
         console.log("New user connected");
 
-        socket.on('join', (params, callback) => {
-            //This should make rooms case insensitive
-            if (params) {
-                params.room = params.room.toUpperCase();
-            }
-            /**Check if the requirements to access a chat are met */
-            if (!isRealString(params.name) || !isRealString(params.room)) {
-                return callback('Name and room name are required');
-            } else if (users.isUserNameTaken(params.name).length > 0) {
-                return callback('Name is already being used');
-            } else {//handle the join event
-                socket.join(params.room);
-                callback();
-                users.removeUser(socket.id);
-                users.addUser(socket.id, params.name, params.room);
+        socket.on('join', joinController);
 
-                io.to(params.room).emit('updateUserList', users.getUsersList(params.room));
-                socket.broadcast.to(params.room).emit("newMessage", generateMessage("Admin", `${params.name} has joined`));
+        socket.on('checkCredentials', checkCredentials);
 
-                socket.emit("newMessage", generateMessage( /*from*/ "Admin", /*message*/ "Wellcome to the chat app!"));
-                callback();
-            }
-        });
+        socket.on("disconnect", disconnectController);
 
-        socket.on('checkCredentials', (params, callback) => {
-            if (!isRealString(params.name) || !isRealString(params.room)) {
-                callback('Name and room name are required');
-            } else if (users.isUserNameTaken(params.name).length > 0) {
-                callback('Name is already being used');
-            } else {
-                callback();
-            }
-        });
+        socket.on("createMessage", createMessageController);
 
-        socket.on("disconnect", () => {
-            console.log("Client disconnected");
-            var user = users.removeUser(socket.id);
-            if (user) {
-                io.to(user.room).emit('updateUserList', users.getUsersList(user.room));
-                io.to(user.room).emit('newMessage', generateMessage('Admin', `The user ${user.name} has left`));
-            }
-        });
+        socket.on('createLocationMessage', locationMessageController);
 
-        socket.on("createMessage", (msg, callback) => {
-            var user = users.getUser(socket.id);
-            if (isRealString(msg.text) && user) {
-                io.to(user.room)
-                    .emit('newMessage', generateMessage(user.name, msg.text)
-                    );
-                //callback is called to tell the client when the server processing is done
-            }
-            callback();
-        });
-
-        socket.on('createLocationMessage', (coords) => {
-            var user = users.getUser(socket.id);
-            io.to(user.room).emit('newLocationMessage', generateLocationMessage(
-                user.name, coords.latitude, coords.longitude
-            ));
-        });
-
-        socket.on('fetchRomms', () => {
-            socket.emit('roomsListAupdate', { rooms: users.getRooms() });
-        })
+        socket.on('fetchRomms', fetchRoomsController)
     });
 }
+
+const joinController  = (params, callback) => {
+    //This should make rooms case insensitive
+    if (params) {
+        params.room = params.room.toUpperCase();
+    }
+    /**Check if the requirements to access a chat are met */
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+        return callback('Name and room name are required');
+    } else if (users.isUserNameTaken(params.name).length > 0) {
+        return callback('Name is already being used');
+    } else {//handle the join event
+        socket.join(params.room);
+        callback();
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+
+        io.to(params.room).emit('updateUserList', users.getUsersList(params.room));
+        socket.broadcast.to(params.room).emit("newMessage", generateMessage("Admin", `${params.name} has joined`));
+
+        socket.emit("newMessage", generateMessage( /*from*/ "Admin", /*message*/ "Wellcome to the chat app!"));
+        callback();
+    }
+}
+
+const checkCredentials = (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+        callback('Name and room name are required');
+    } else if (users.isUserNameTaken(params.name).length > 0) {
+        callback('Name is already being used');
+    } else {
+        callback();
+    }
+}
+
+const disconnectController = () => {
+    console.log("Client disconnected");
+    var user = users.removeUser(socket.id);
+    if (user) {
+        io.to(user.room).emit('updateUserList', users.getUsersList(user.room));
+        io.to(user.room).emit('newMessage', generateMessage('Admin', `The user ${user.name} has left`));
+    }
+};
+
+const createMessageController = (msg, callback) => {
+    var user = users.getUser(socket.id);
+    if (isRealString(msg.text) && user) {
+        io.to(user.room)
+            .emit('newMessage', generateMessage(user.name, msg.text)
+            );
+        //callback is called to tell the client when the server processing is done
+    }
+    callback();
+};
+
+const locationMessageController = (coords) => {
+    var user = users.getUser(socket.id);
+    io.to(user.room).emit('newLocationMessage', generateLocationMessage(
+        user.name, coords.latitude, coords.longitude
+    ));
+};
+
+const fetchRoomsController = () => {
+    socket.emit('roomsListAupdate', { rooms: users.getRooms() });
+};
